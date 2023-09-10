db35f510b32667ddd435c787f57b602daa4b1452
Gustavo Laitano <gu.laitano@hotmail.com>
2023-09-10 18:07:53 -0300

    Projeto criado

---
+++ XadrezConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace XadrezConsole
+{
+    class XadrezConsole
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
